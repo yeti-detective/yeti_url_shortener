ee560c111bf8812d99c8fc5c4a8c65dfdc82f536
Christopher M Brown <yeti.detective@gmail.com>
2017-01-03 18:33:44 +0000

    second commit. I am a failure

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
+var validUrl = require('valid-url');
 
-app.get("/:url", function(req, res){
-    var url = req.params.url
+app.get("/short/:url", function(req, res){
+    var url = req.params.url;
+    res.send(url);
+});
+
+app.get('*', function(req, res){
+    res.send('Add a URL as n argument to the web address');
+});
+
+app.listen(8080,function(){
+    console.log('server listening on port 8080');
 })
