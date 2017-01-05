08f1cdcd3ff94a6945b14b382efbb46c2619d849
Christopher M Brown <yeti.detective@gmail.com>
2017-01-05 18:22:05 +0000

    Heck yeah, URL validation

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ var express = require('express');
 var app = express();
 var validUrl = require('valid-url');
 
-app.get("/short/:url", function(req, res){
-    var url = req.params.url;
-    res.send(url);
+app.get("/:input", function(req, res){
+    if (Boolean(parseInt(req.params.input))){
+        res.send(req.params.input);
+    }
+});
+
+app.get('/http:\//:url', function(req, res){
+    if(Boolean(validUrl.isUri('http://' + req.params.url))){
+        res.send("Mongo time");
+    }
+});
+
+app.get('/https:\//:urlSSH', function(req, res){
+    if(Boolean(validUrl.isUri('https://' + req.params.urlSSH))){
+        res.send("Mongo time");
+    }
 });
 
 app.get('*', function(req, res){
-    res.send('Add a URL as n argument to the web address');
+    res.send('Add a URL as an argument to the web address');
 });
 
 app.listen(8080,function(){
